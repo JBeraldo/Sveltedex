5787c23f11e9ea4d8314a8a660688221b505df6d
Beraldo <49615542+JBeraldo@users.noreply.github.com>
2022-06-26 02:58:10 -0300

    Finalizado

---
+++ src/routes/about.svelte
@@ -0,0 +1,5 @@
+<svelte:head>
+    <title>Sveltedex</title>
+</svelte:head>
+<h1 class="text-4xl text-center my-8 uppercase">Sobre</h1>
+<p>Este e um site feito em Svelte com intuito de aprendizado sobre o "Framework"</p>
--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,2 +1,52 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script context="module">
+	export async function load({ params }) {
+        const url = `https://pokeapi.co/api/v2/pokemon?limit=150&offset=0`;
+		const res = await fetch(url);
+		const data = await res.json();
+		const loadedPokemon = data.results.map((data, index) => {
+			return {
+				name: data.name,
+				id: index + 1,
+				image: `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${
+					index + 1
+				}.png`
+			};
+		});
+        return {props: {pokemons:loadedPokemon}}
+	}
+</script>
+
+<script>
+	import PokemanCard from '../components/pokemanCard.svelte';
+	export let pokemons;
+	let searchTerm = '';
+	let filteredPokemons = [];
+
+	$: {
+		if (searchTerm) {
+			filteredPokemons = pokemons.filter((pokeman) =>
+				pokeman.name.toLowerCase().includes(searchTerm.toLocaleLowerCase())
+			);
+		} else {
+			filteredPokemons = [...pokemons];
+		}
+	}
+</script>
+
+<svelte:head>
+	<title>Sveltedex</title>
+</svelte:head>
+<h1 class="text-4xl text-center my-8 uppercase">Sveltedex</h1>
+
+<input
+	bind:value={searchTerm}
+	class="w-full rounded-md text-lg p-4 border-2 border-gray-200"
+	type="text"
+	placeholder="Digite o nome do pokemon..."
+/>
+
+<div class="py-4 grid gap-4 md:grid-cols-2 grid-cols-1">
+	{#each filteredPokemons as pokeman}
+		<PokemanCard {pokeman} />
+	{/each}
+</div>
